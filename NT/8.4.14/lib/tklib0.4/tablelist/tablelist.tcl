18ef00a0142d36677d127d4bd43c94da285f23b2
Thomas Ritz <tritz@infopark.de>
2008-08-28 14:13:21 +0200

    init

---
+++ NT/8.4.14/lib/tklib0.4/tablelist/tablelist.tcl
@@ -0,0 +1,14 @@
+#==============================================================================
+# Main Tablelist package module.
+#
+# Copyright (c) 2000-2006  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+#==============================================================================
+
+package require Tcl 8
+package require Tk  8
+package require tablelist::common
+
+package provide Tablelist 4.5
+package provide tablelist 4.5
+
+::tablelist::useTile 0
